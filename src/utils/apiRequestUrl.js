afb0c9b1bf67a634c412f02ebb14fd25e00a26b8
joshbaz <jkimbareeba@yahoo.com>
2025-04-01 02:14:00 +0300

    prod-url change

--- src/utils/apiRequestUrl.js
+++ src/utils/apiRequestUrl.js
@@ -1,6 +1,7 @@
 import {queryClient} from './tanstack'
 import axios from 'axios'
-export const BASE_API_URL = 'http://localhost:5000/api/v1';
+// export const BASE_API_URL = 'http://localhost:5000/api/v1';
+export const BASE_API_URL = 'http://138.68.132.20/api/v1';
 
 export const socketUrl = "localhost:5000"
 
