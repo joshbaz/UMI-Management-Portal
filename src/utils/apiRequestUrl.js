a55e0a12690a84d4339a4289c8eba1c66b741158
joshbaz <jkimbareeba@yahoo.com>
2025-04-03 01:47:05 +0300

    updates to url

--- src/utils/apiRequestUrl.js
+++ src/utils/apiRequestUrl.js
@@ -1,7 +1,7 @@
 import {queryClient} from './tanstack'
 import axios from 'axios'
-export const BASE_API_URL = 'http://localhost:5000/api/v1';
-// export const BASE_API_URL = 'https://umiapi.alero.co.ke/api/v1';
+// export const BASE_API_URL = 'http://localhost:5000/api/v1';
+export const BASE_API_URL = 'https://umiapi.alero.co.ke/api/v1';
 
 export const socketUrl = "localhost:5000"
 
